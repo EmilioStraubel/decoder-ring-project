217f74770ef1fcc2289a5e16358938efe3ff6ab5
Emilio Schmidt <86331265+EmilioSchmidt@users.noreply.github.com>
2021-07-22 19:34:41 -0400

    Update polybius.js

--- Project_Decoder_Ring_1/src/polybius.js
+++ Project_Decoder_Ring_1/src/polybius.js
@@ -7,26 +7,25 @@ const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
   function polybius(input, encode = true) {
-    
-    const cipher = {                                                                      //key-value pairs for alphabet value in cipher
+    //key-value pairs for alphabet value in cipher    
+    const cipher = {                                                                      
       a: "11", b: "21", c: "31", d: "41", e: "51",
       f: "12", g: "22", h: "32", i: "42", j: "42", k: "52",
       l: "13", m: "23", n: "33", o: "43", p: "53",
       q: "14", r: "24", s: "34", t: "44", u: "54",
-      v: "15", w: "25", x: "35", y: "45", z: "55", [' ']: " "                             //saying that an empty space should be assigned an empty space
+      v: "15", w: "25", x: "35", y: "45", z: "55", [' ']: " "  //saying that an empty space should be assigned an empty space
     };
-    
-    const decoded = {                                                                     //key-value pairs for what each cipher number represents in alphabet
+ //key-value pairs for what each cipher number represents in alphabet
+    const decoded = {                                                                    
      11: "a", 12: "f", 13: "l", 14: "q", 15: "v", 21: "b", 22: "g", 23: "m", 
      24: "r", 25: "w", 31: "c", 32: "h", 33: "n", 34: "s", 35: "x", 41: "d", 
      42: "(i/j)", 43: "o", 44: "t", 45: "y", 51: "e", 52: "k", 53: "p",
      54: "u", 55: "z", [' ']: " "
     };
-    
-    
-    if(typeof input !== 'string') return false;                                            // if input is not a string, return false
-    let lowerCaseInput = input.toLowerCase()                                               //making entire input to lowercase so that we can ignore capital letters
-
+    // if input is not a string, return false    
+    if(typeof input !== 'string') return false;
+     //making entire input to lowercase so that we can ignore capital letters    
+    let lowerCaseInput = input.toLowerCase()                                              
     
     if(encode) {                                                                           //if encoding, use cipher
       return lowerCaseInput.split('').map((letter) => {                                    //splitting our lowerCaseInput to loop over it. Creates an array of individual characters. We map over array of letters to create new array with results of the callback fn 
