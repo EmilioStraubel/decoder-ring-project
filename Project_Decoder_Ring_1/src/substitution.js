9ab7497a1368bc3b0d4a81e5f46cf2ca0f4cd264
Emilio Schmidt <86331265+EmilioSchmidt@users.noreply.github.com>
2021-07-22 21:29:11 -0400

    Update substitution.js

--- Project_Decoder_Ring_1/src/substitution.js
+++ Project_Decoder_Ring_1/src/substitution.js
@@ -20,27 +20,28 @@ const substitutionModule = (function () {
       if(duplicate.indexOf(alphabet[character]) < 0) {               
         duplicate.push(alphabet[character])
       }
-      else {                                                            //if the indexOf returns a value that is > -1 (meaning that the letter is already present and thus a duplicate), return false.
+ //if the indexOf returns a value that is > -1 (meaning that the letter is already present and thus a duplicate), return false.
+      else {                                                          
         return false
       }
     }
 
 
-    let abc = "abcdefghijklmnopqrstuvwxyz ".split('')                  //capturing abc's in a variable as an array of individual characters using the split method. Includes intentional space.//Splitting our abc to loop over it. Creates an array of individual characters. 
-    let altAbc = [...alphabet, " "];                                   //includes space to alternative abc.
-    let lowerCaseInput = input.toLowerCase().split('')                 //taking input passed into function and making it all lowercase so that we don't have to worry about capital letters.//Splitting our lowerCaseInput to loop over it. Creates an array of individual characters. 
+    let abc = "abcdefghijklmnopqrstuvwxyz ".split('')   //capturing abc's in a variable as an array of individual characters using the split method. Includes intentional space.//Splitting our abc to loop over it. Creates an array of individual characters. 
+    let altAbc = [...alphabet, " "];                    //includes space to alternative abc.
+    let lowerCaseInput = input.toLowerCase().split('') //taking input passed into function and making it all lowercase so that we don't have to worry about capital letters.//Splitting our lowerCaseInput to loop over it. Creates an array of individual characters. 
     
     
    
-    if (encode === true) {                                //if we are encoding
-     return lowerCaseInput.map((letter) => {             //We map over array of letters to create new array with results of the callback fn
-        return altAbc[abc.indexOf(letter)]               //Accessing altAbc array(ie: if altAbc = xoyqmcgrukswaflnthdjpzibev, then altAbc[0] is "x". Returning the element in altAbc array at the position of the index of the letter in the abc array.
-      }).join('')                                       //bringing individual characters back together
+    if (encode === true) {                      //if we are encoding
+     return lowerCaseInput.map((letter) => {    //We map over array of letters to create new array with results of the callback fn
+        return altAbc[abc.indexOf(letter)]      //Accessing altAbc array(ie: if altAbc = xoyqmcgrukswaflnthdjpzibev, then altAbc[0] is "x". Returning the element in altAbc array at the position of the index of the letter in the abc array.
+      }).join('')                               //bringing individual characters back together
     }
-    else {                                             //If we are decoding
-      return lowerCaseInput.map((letter) => {         //We map over array of letters to create new array with results of the callback fn
-        return abc[altAbc.indexOf(letter)]           //Accessing altAbc array(ie: if abc = "abcdefghijklmnopqrstuvwxyz ", then abc[0] is "a". Returning the element in abc array at the position of the index of the letter in the altAbc array.
-      }).join('')                                    //bringing individual characters back together
+    else {                                      //If we are decoding
+      return lowerCaseInput.map((letter) => {   //We map over array of letters to create new array with results of the callback fn
+        return abc[altAbc.indexOf(letter)]      //Accessing altAbc array(ie: if abc = "abcdefghijklmnopqrstuvwxyz ", then abc[0] is "a". Returning the element in abc array at the position of the index of the letter in the altAbc array.
+      }).join('')                              //bringing individual characters back together
     }
   }
 
